2ae8d217f6897e6d674511b996b1d3a767cfe386
Amanda Segal <amandarose31@gmail.com>
2018-03-06 03:48:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,7 @@ addElementToBeginningOfArray = ["thing3",...addElementToBeginningOfArray]
 var destructivelyAddElementToBeginningOfArray = ["thing5", "thing6"]
 
 destructivelyAddElementToBeginningOfArray.push("thing7")
+
+var destructivelyRemoveElementFromBeginningOfArray = ["day1","day2","day3"]
+
+destructivelyRemoveElementFromBeginningOfArray.shift();
