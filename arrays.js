8ddddb045a3e62c6cdab56d3e82382030658bc7b
Amanda Segal <amandarose31@gmail.com>
2018-03-06 03:38:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,7 @@
-var chocolateBars = [snickers, hundred grand, kitkat, skittles]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+var addElementToBeginningOfArray = ["thing1", "thing2"]
+
+["thing3",...addElementToBeginningOfArray]
+
+var destructivelyAddElementToBeginningOfArray
